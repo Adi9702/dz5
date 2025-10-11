c8a1529368866a030e74b4577f72abf45896ecf5
Ади9702 <adalatarapova@gmail.com>
2025-10-11 16:24:45 +0600

    d5

--- 30-1B/1 месяц/lesson_3_dz.py
+++
@@ -1,9 +0,0 @@
-
-names = ["Aslan", "Islam", "Bael", "Askhat", "Aruzhan"]
-operator = input("Введите имя: ")
-if operator in names:
-    print("Имя найдено:", operator )
-else:
-    names.append(operator)
-    print("Имя добавлено:", operator)
-    print("Обновленный список имен:", names)
--- 30-1B/1 месяц/lesson_6_dz.py
+++
@@ -1,7 +0,0 @@
-
-def greet_users(name: str) -> str:
-    print("Здравствуйте,", name)
-
-greet_users("Аслан!")
-greet_users("Диана!")
-greet_users("Нурбек!")
